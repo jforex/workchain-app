c42d5a5c8aac6821d9832e826cd67b979db3cea8
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2026-04-09 12:59:50 +0100

    fix: lib/supabase.ts in correct location

--- app/jobs/lib/supabase.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.https://blheublmkgsiottnxtck.supabase.co
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsaGV1Ymxta2dzaW90dG54dGNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MjQ4NTIsImV4cCI6MjA5MTMwMDg1Mn0.ZGski1cdBpnbeB8-1qXi_2L9CQzqgzikWgKE_uSv6pQ
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
