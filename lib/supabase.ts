ca77bafcca9af9df6129ecf5d0c54b7d9500049d
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2026-04-09 18:26:02 +0100

    fix: supabase client clean

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.https://blheublmkgsiottnxtck.supabase.co
-const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsaGV1Ymxta2dzaW90dG54dGNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MjQ4NTIsImV4cCI6MjA5MTMwMDg1Mn0.ZGski1cdBpnbeB8-1qXi_2L9CQzqgzikWgKE_uSv6pQ
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  process.env.https://blheublmkgsiottnxtck.supabase.co!,
+  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsaGV1Ymxta2dzaW90dG54dGNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MjQ4NTIsImV4cCI6MjA5MTMwMDg1Mn0.ZGski1cdBpnbeB8-1qXi_2L9CQzqgzikWgKE_uSv6pQ!
+)
