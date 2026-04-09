e5e200a406d19503f08b89fcf9130dda48af1923
MACBOOK <macbook@MACBOOKs-MacBook-Pro.local>
2026-04-09 18:43:01 +0100

    fix: supabase client correct

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  process.env.https://blheublmkgsiottnxtck.supabase.co!,
-  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsaGV1Ymxta2dzaW90dG54dGNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MjQ4NTIsImV4cCI6MjA5MTMwMDg1Mn0.ZGski1cdBpnbeB8-1qXi_2L9CQzqgzikWgKE_uSv6pQ!
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
